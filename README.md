cec148997c533f10477db55772bd59faef2d6907
Zied Ben Amor <zied-ci@hotmail.fr>
2022-09-20 13:57:25 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# Capstone1
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Conference Offical Web Page
+
+> Capstone Project for Module 1 (Microverse)
+
+
+## Built With
+
+- HTML/CSS/JS 
+- NodeJS 
+- Linters 
+
+## Live Demo (if available)
+
+[Live Demo Link](https://livedemo.com)
+
+
+## Getting Started
+
+
+
+### Prerequisites
+
+### Setup
+
+### Install
+
+### Usage
+
+### Run tests
+
+### Deployment
+
+
+
+## Authors
+
+👤 **Author**
+
+- GitHub: [@github](https://github.com/zied2112)
+- Twitter: [@twitter](https://twitter.com/AmorZied1996)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/zied-ben-amor-924908149/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ js/speakersData.js
@@ -0,0 +1,51 @@
+const speakers = [
+  {
+    id: '1',
+    name: 'Yosri Ghorbel',
+    role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
+    description:
+      'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    image: '../images/speakers/speaker_01.png',
+  },
+  {
+    id: '2',
+    name: 'Yosri Ghorbel',
+    role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
+    description:
+      'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    image: '../images/speakers/speaker_02.png',
+  },
+  {
+    id: '3',
+    name: 'Yosri Ghorbel',
+    role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
+    description:
+      'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    image: '../images/speakers/speaker_03.png',
+  },
+  {
+    id: '4',
+    name: 'Yosri Ghorbel',
+    role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
+    description:
+      'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    image: '../images/speakers/speaker_04.png',
+  },
+  {
+    id: '5',
+    name: 'Yosri Ghorbel',
+    role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
+    description:
+      'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    image: '../images/speakers/speaker_05.png',
+  },
+  {
+    id: '6',
+    name: 'Yosri Ghorbel',
+    role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
+    description:
+      'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    image: '../images/speakers/speaker_06.png',
+  },
+];
+export default speakers;
