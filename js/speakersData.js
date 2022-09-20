30b033eea34074911f17b1443f9a719ac092afe0
Zied Ben Amor <zied-ci@hotmail.fr>
2022-09-20 14:25:02 +0200

    fix image issue

--- js/speakersData.js
+++ js/speakersData.js
@@ -5,7 +5,7 @@ const speakers = [
     role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
     description:
       'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    image: '/images/speakers/speaker1.png',
+    image: './images/speakers/speaker1.png',
   },
   {
     id: '2',
@@ -13,7 +13,7 @@ const speakers = [
     role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
     description:
       'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    image: '/images/speakers/speaker2.png',
+    image: './images/speakers/speaker2.png',
   },
   {
     id: '3',
@@ -21,7 +21,7 @@ const speakers = [
     role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
     description:
       'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    image: '/images/speakers/speaker3.png',
+    image: './images/speakers/speaker3.png',
   },
   {
     id: '4',
@@ -29,7 +29,7 @@ const speakers = [
     role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
     description:
       'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    image: '/images/speakers/speaker4.png',
+    image: './images/speakers/speaker4.png',
   },
   {
     id: '5',
@@ -37,7 +37,7 @@ const speakers = [
     role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
     description:
       'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    image: '/images/speakers/speaker5.png',
+    image: './images/speakers/speaker5.png',
   },
   {
     id: '6',
@@ -45,7 +45,7 @@ const speakers = [
     role: 'Professor of Entrepreneurial Legal Studies at Havard Law School',
     description:
       'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    image: '/images/speakers/speaker6.png',
+    image: './images/speakers/speaker6.png',
   },
 ];
 export default speakers;
